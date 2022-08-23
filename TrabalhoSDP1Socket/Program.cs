4d0fb9e7169287630cc0ef006933ea2708cc31af
crickkin <37256927+crickkin@users.noreply.github.com>
2022-08-23 15:39:47 -0300

    :tada: first commit

---
+++ TrabalhoSDP1Socket/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace TrabalhoSDP1Socket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            connection:
+            try
+            {
+                //
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("error connecting to server...");
+                Console.WriteLine("\n\n============\n\n" + e.Message + "\n\n============\n\n");
+                Console.WriteLine("press any key to try again.");
+                Console.ReadLine();
+                goto connection;
+            }
+        }
+    }
+}
